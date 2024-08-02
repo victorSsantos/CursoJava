ea38ee21d944b21b29d0b78e44bd17419b7fb973
Victor Serrano <victor.ssantos089@gmail.com>
2024-08-02 10:24:14 -0300

    exercicio fixacao enumerators

--- Enumerators/src/entities/Order.java
+++ Enumerators/src/entities/Order.java
@@ -1,38 +1,22 @@
 package entities;
 
 import entities.enums.OrderStatus;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Order {
-
-    private Integer id;
-    private LocalDateTime date;
+    private final Date date;
     private OrderStatus status;
+    private final Client client;
+    private final List<OrderItem> orderItems;
 
-    public Order() {
-    }
-
-    public Order(Integer id, LocalDateTime date, OrderStatus status) {
-        this.id = id;
+    public Order(Date date, OrderStatus status, Client client) {
         this.date = date;
         this.status = status;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime moment) {
-        this.date = moment;
+        this.client = client;
+        this.orderItems = new ArrayList<>();
     }
 
     public OrderStatus getStatus() {
@@ -43,9 +27,33 @@ public class Order {
         this.status = status;
     }
 
-    @Override
-    public String toString() {
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-        return "Order [id=" + id + ", date=" + fmt.format(date) + ", status=" + status + ']';
+    public void addItem(OrderItem item){
+        orderItems.add(item);
+    }
+
+    public void removeItem(OrderItem item){
+        orderItems.remove(item);
+    }
+
+    Double total(){
+        Double total = 0.0;
+        for(OrderItem item : orderItems){
+            total+=item.subTotal();
+        }
+        return total;
+    }
+
+    public void printSummary(){
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        System.out.println();
+        System.out.println("ORDER SUMMARY:");
+        System.out.println("Order moment: " + fmt.format(date));
+        System.out.println("Order status: " + status);
+        System.out.println("Client: " + client.toString());
+        System.out.println("Order items:");
+        for(var item : orderItems) {
+            System.out.println(item.toString());
+        }
+        System.out.println("Total price: " + String.format("%.2f", total()));
     }
 }
